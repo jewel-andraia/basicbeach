8011b7a8a2263d908195b6077e9590b05a42d17a
Andy Tuba <andytuba@andytuba.com>
2020-07-03 15:10:02 -0700

    all the plumbing except piping flattened text into scad render

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ Designing earrings using procedural generation and 3-D modeling tools such as Tr
 
 Brainstorm tweetstorm: https://twitter.com/andytuba/status/1264277315011227655
 
+
+# External dependencies
+
+openscad app
+
+# Troubleshooting
+
+> Fontconfig warning: "/Applications/OpenSCAD.app/Contents/Resources/fonts/fonts.conf", line 84: unknown element "blank"
+
+https://github.com/openscad/openscad/issues/2888
+I've taken to simply removing the <blank>...</blank> segment from that file, as it seems to have no impact.
+
--- src/index.js
+++ src/index.js
@@ -1,33 +1,71 @@
 const tracery = require('tracery-grammar');
 const nodescad = require('nodescad');
 const seedrandom = require('seedrandom');
+const fs = require('fs');
 
-/* Constants */
+/* Config */
 const config = {
-	seed: Math.random() * 1000,
+	seed: Math.floor(Math.random() * 1000),
 	grammar: 't21-tracery-readme', // 'basicbeach',
+	model: 'example', // 'basicbeach',
+	modelPath: './src/models',
 	nodescad: {
-			'binaryPath': '/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD',
-			'inputFile': `./src/models/example.scad`,
+		binaryPath: '/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD',
+		render: true,
+	},
+	variables: {
 	},
 };
 
-/* Main */
+/* Calculated constants */
+const outputPath = `${process.cwd()}/output/${config.grammar}/${config.model}/${config.seed}/${config.grammar} - ${config.model} - ${config.seed} - ${Date.now()}`;
+const outputFilenameStub = `${config.grammar} - ${config.model} - ${config.seed}`;
 
+/* Prepare output files */
+fs.mkdirSync(outputPath, { recursive: true });
+fs.writeFileSync(`${outputPath}/config - ${config.grammar} - ${config.model} - ${config.seed}.json`, JSON.stringify(config, undefined, 2));
+
+/* Generate text */
 seedrandom(config.seed, { global: true });
 const grammar = tracery.createGrammar(require(`./grammar/${config.grammar}.json`));
 grammar.addModifiers(tracery.baseEngModifiers);
-
 const text = grammar.flatten('#origin#');
 
-console.log({
-	...config,
-	text,
-});
-nodescad.render(config.nodescad, function (result, error) {
-	if (error) {
-		console.error(error);
+/* Render scad script + text into stl model andpng */
+const nodescadOptions = {
+	...config.nodescad,
+	inputFile: `"${config.modelPath}/${config.model}.scad"`,
+	/*
+	variables: {
+		...config.variables,
+		text,
 	}
+	*/
+};
+const stlOptions = {
+	... nodescadOptions,
+	format: 'stl',
+	outputFile: `"${outputPath}/stl - ${outputFilenameStub}.stl"`
+};
+const pngOptions = {
+	...nodescadOptions,
+	format: 'png',
+	outputFile: `"${outputPath}/png - ${outputFilenameStub}.png"`
+};
 
-	console.log(result);
-})
+nodescad.render(stlOptions, function (err, result) {
+	if (err || result.stderr) {
+		throw err || result.stderr;
+	}
+});
+nodescad.render(pngOptions, function (err, result) {
+	if (err || result.stderr) {
+		throw err || result.stderr;
+	}
+});
+
+/* Ding, fries are done. */
+console.info({
+	config,
+	outputPath,
+});
