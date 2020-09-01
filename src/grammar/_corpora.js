f28956e40762cf81957b32eb5ca47bb58a30c8f1
andytuba <andytuba@andytuba.com>
2020-09-01 06:53:43 +0000

    apparently some corpora have _ in the keys/filenames

--- src/grammar/_corpora.js
+++ src/grammar/_corpora.js
@@ -1,14 +1,18 @@
 
 var corpora = require('corpora-project');
 
-const corporaRE = /^corpora-(\w+)-(\w+)$/;
+const corporaRE = /^corpora-(\w+)-([\w_]+)(?:-([\w_]+))?$/;
 
 function preprocessor(grammar) {
     for (let key in grammar) {
         const match = corporaRE.exec(key);
         if (match) {
-            grammar[key] = corpora.getFile(match[1], match[2])[match[1]]
+            grammar[key] = corpora.getFile(match[1], match[2])[match[3]]
+		|| corpora.getFile(match[1], match[2])[match[1]]
 		|| corpora.getFile(match[1], match[2])[match[2]]
+	    if (!grammar[key]) {
+		console.error(" couldn't find a corpora-projects match for", match);
+		}
         }
     }
 
