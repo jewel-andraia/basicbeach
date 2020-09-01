cf8da7ae025c35a829483182e4f5d7230f2d5939
andytuba <andytuba@andytuba.com>
2020-09-01 06:14:53 +0000

    quick refactor of traceryHtml so it's easier to make an index page

--- src/www.js
+++ src/www.js
@@ -13,64 +13,17 @@ const server = http.createServer((req, res) => {
 		const reqUrl = url.parse(req.url, true);
 		console.log({ reqUrl });
 		const path = reqUrl.path.match(/^\/(?<prefix>\w+)(?:\/(?<grammar>[\w\-]+))?/);
-		const grammar = path && path.groups.grammar || 't21-tracery-readme';
+		const grammar = path && path.groups.grammar;
 		const seed = parseInt(reqUrl.query.seed, 10) || Math.floor(Math.random() * 99999);
 
 		console.debug({ reqUrl, grammar });
+
+		
+
+
 		let html = '<html>honk';
 		try {
-			const traceryOutput = generateText({
-				grammar,
-				seed,
-			});
-			html = `
-			<html>
-				<head>
-					<title>/tracery/${grammar}</title>
-					<style>
-						html {
-							background-color: cornflowerblue;
-							color: antiquewhite;
-							font-size: 5vw;
-						}
-						body {
-							padding: 3%;
-						}
-
-						h1 {
-							font-family: sans-serif;
-							font-style: oblique;
-							font-size: .8rem;
-							text-align: right;
-							text-transform: capitalize;
-						}
-						a {
-							color: inherit;
-							text-decoration: none;
-						}
-
-						pre {
-							font-family: serif;	
-							font-size: 1rem;
-							width: 80%;
-							white-space: pre-wrap;
-						}
-
-
-						footer {
-							font-size: .5rem;
-							text-align: right;
-						}
-
-					</style>
-				</head>
-				<body>
-					<h1><a href="?">${deslug(grammar)}</a> <a href="?seed=${traceryOutput.config.seed}">&#x1f517;</a></h1>
-					<pre>${traceryOutput.output.text}</pre>
-					<footer><a href="https://github.com/andytuba/basicbeach" target="_blank">a tracery project</a> by <a href="https://twitter.com/andytuba" target="_blank">@andytuba</a></footer>
-				</body>
-			</html>
-				`;
+			html = traceryHtml({ grammar, seed });
 		} catch (err) {
 			if (err.code === 'MODULE_NOT_FOUND') {
 				res.statusCode = 404;
@@ -95,6 +48,62 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+function traceryHtml({ grammar = '', seed }) {
+	const traceryOutput = generateText({
+		grammar: grammar || 't21-tracery-readme',
+		seed,
+	});
+	html = `
+<html>
+	<head>
+		<title>/tracery/${grammar}</title>
+		<style>
+			html {
+				background-color: cornflowerblue;
+				color: antiquewhite;
+				font-size: 5vw;
+			}
+			body {
+				padding: 3%;
+			}
+
+			h1 {
+				font-family: sans-serif;
+				font-style: oblique;
+				font-size: .8rem;
+				text-align: right;
+				text-transform: capitalize;
+			}
+			a {
+				color: inherit;
+				text-decoration: none;
+			}
+
+			pre {
+				font-family: serif;	
+				font-size: 1rem;
+				width: 80%;
+				white-space: pre-wrap;
+			}
+
+
+			footer {
+				font-size: .5rem;
+				text-align: right;
+			}
+
+		</style>
+	</head>
+	<body>
+		<h1><a href="?">${deslug(grammar)}</a> <a href="?seed=${traceryOutput.config.seed}">&#x1f517;</a></h1>
+		<pre>${traceryOutput.output.text}</pre>
+		<footer><a href="https://github.com/andytuba/basicbeach" target="_blank">a tracery project</a> by <a href="https://twitter.com/andytuba" target="_blank">@andytuba</a></footer>
+	</body>
+</html>
+	`;
+	return html;
+}
 
 function generateText(config) {
 	/* Generate text */
