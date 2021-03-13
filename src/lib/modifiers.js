c910ac96b7543ab8b1264b153b19738eb067b281
andytuba <andytuba@andytuba.com>
2021-03-13 01:58:21 +0000

    broken

--- src/lib/modifiers.js
+++ src/lib/modifiers.js
@@ -4,4 +4,29 @@ const htmlEntity = exports.htmlEntity = function(s) {
 	return emojiUnicode(s).split(' ').map(u => `&\\#x${u};`).join('');
 };
 
+const ordinal = exports.ordinal = function(s) {
+	switch (s.substr(s.length - 1)) {
+		case "1": {
+			return s + 'st';
+		}
+		case "2": {
+			return s + 'nd';
+		}
+		case "3": {
+			return s + 'rd';
+		}
+		case "4":
+		case "5":
+		case "6":
+		case "7":
+		case "8":
+		case "9":
+		case "0":
+		{
+			return s + 'th';
+		}
+		default:
+			return s;
+	};
+};
 
